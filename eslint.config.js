c79ac2404b8e246a6e36fecdf0d97a93597c16d9
Brandon Watkins <bwat47@gmail.com>
2025-09-20 10:39:54 -0400

    fix eslint
    project was using eslint v9 with the old config format, so npm run lint wasn't working
    
    migrated to flat config

--- eslint.config.js
+++ eslint.config.js
@@ -1,31 +1,43 @@
-export default {
-  env: {
-    node: true,
-    es2022: true
-  },
-  extends: ['eslint:recommended'],
-  parserOptions: {
-    ecmaVersion: 2022,
-    sourceType: 'module'
-  },
-  rules: {
-    'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    'no-console': 'warn',
-    'prefer-const': 'error',
-    'no-var': 'error'
-  },
-  overrides: [
-    {
-      files: ['test/**/*.js'],
-      env: {
-        node: true
-      },
+import js from '@eslint/js'
+import globals from 'globals'
+
+const baseLanguageOptions = {
+  ecmaVersion: 2022,
+  sourceType: 'module',
+  globals: {
+    ...globals.node
+  }
+}
+
+export default [
+  {
+    ...js.configs.recommended,
+    languageOptions: {
+      ...js.configs.recommended.languageOptions,
+      ...baseLanguageOptions,
       globals: {
+        ...(js.configs.recommended.languageOptions?.globals ?? {}),
+        ...baseLanguageOptions.globals
+      }
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      'no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      'no-console': 'warn',
+      'prefer-const': 'error',
+      'no-var': 'error'
+    }
+  },
+  {
+    files: ['test/**/*.js'],
+    languageOptions: {
+      globals: {
+        ...globals.node,
         describe: 'readonly',
         it: 'readonly',
         expect: 'readonly',
         beforeEach: 'readonly'
       }
     }
-  ]
-} 
+  }
+]
